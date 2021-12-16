d69d49b091333633f4e7cbe0c4b82149950544e7
Kirsty Brown <kirsty.a.d.brown@gmail.com>
2021-12-16 12:54:07 +0100

    feat: added challenge 15

---
+++ 15-PerfectJump.js
@@ -0,0 +1,55 @@
+function checkSledJump(heights) {
+    const maxHeight = Math.max(...heights);
+    const maxHeightIndex = heights.indexOf(maxHeight);
+
+    // sleigh never takes off
+    if (maxHeight === 0) {
+      return false;
+    }
+
+    // sleigh goes up but never comes down
+    if (maxHeightIndex === (heights.length - 1)) {
+      return false;
+    }
+
+    // sleigh goes up again after highest jump
+    for (let i = maxHeightIndex; i >= 0; i--) {
+      if (heights[i - 1] >= heights[i]) {
+        return false;
+      }
+    }
+
+    // sleigh down before reaching highest point
+    for (let i = maxHeightIndex + 1; i < heights.length; i++) {
+      if (heights[i] > heights[i - 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  console.log(checkSledJump([0, 1, 0]));
+  console.log(checkSledJump([0, 3, 2, 1]));
+  console.log(checkSledJump([0, 1000, 1]));
+  console.log(checkSledJump([2, 4, 4, 6, 2]));
+  console.log(checkSledJump([1, 2, 3]));
+  console.log(checkSledJump([1, 2, 3, 2, 1, 2, 3]));
+
+  /* Instructions:
+  ¡Estamos haciendo los últimos ajustes para el trineo de Santa Claus!
+
+Como ya sabes, el trineo es volador y estamos ajustando el motor para que haga parabolas lo más óptimas posibles. Para esto el salto debe ser siempre hacia arriba y, a partir del punto más alto, debe bajar siempre hacia abajo...
+
+Nuestro mecánico de confianza, Kiko Belfs, que tiene un Tesla genial, nos ha explicado que los saltos se pueden ver como arrays... y que sólo tenemos que asegurarnos que los números suben y bajan de forma correcta. También nos avisa que sólo pasaremos arrays de, como mínimo, tres posiciones.
+
+Nos ha pasado algunos ejemplos de cómo debería ser nuestra función y algunos resultados:
+
+checkSledJump([1, 2, 3, 2, 1]) // true: sube y baja de forma estricta
+checkSledJump([0, 1, 0]) // -> true: sube y baja de forma estricta
+checkSledJump([0, 3, 2, 1]) // -> true: sube y baja de forma estricta
+checkSledJump([0, 1000, 1]) // -> true: sube y baja de forma estricta
+
+checkSledJump([2, 4, 4, 6, 2]) // false: no sube de forma estricta
+checkSledJump([1, 2, 3]) // false: sólo sube
+checkSledJump([1, 2, 3, 2, 1, 2, 3]) // false: sube y baja y sube... ¡no vale!
+Lo importante: recorrer el array de izquierda a derecha para ver que la subida es siempre estricta, detectar el punto más alto y entonces ver que la bajada es estricta hacia abajo...
+*/
